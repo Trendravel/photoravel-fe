1bd05d94944fc77fd8695ae3063fc5645c0b46bb
Spooder <yoshi7591@naver.com>
2024-08-09 15:58:50 +0900

    feat: 현위치 버튼 색상 비동기 처리

--- src/components/PanButton.tsx
+++ src/components/PanButton.tsx
@@ -7,23 +7,37 @@ import CrossHairLogo from "../assets/crosshair.png"
 const PanButton = (props: { setPos: (center: PanPosition) => void }) => {
     const [isClicked, setIsClicked] = useState(false);
 
-    const SetPosition = () => {
+    const setPosition = async () => {
         setIsClicked(true);
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((pos) => {   
-                props.setPos({center: {
+
+        const getCurrentPosition = () => {
+            return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+        };
+    
+        try {
+            const pos = await getCurrentPosition();
+            props.setPos({
+                center: {
                     lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                }})
-            })
-        } else {
+                    lng: pos.coords.longitude,
+                },
+            });
+        } catch (error) {
             alert("현재 위치를 불러올 수 없는 기기입니다.");
+        } finally {
+            // 위치 처리와 상관없이 마지막에 클릭 상태를 리셋합니다.
+            setTimeout(() => {
+                setIsClicked(false)
+            }, 500)
+            
         }
-        setIsClicked(false);
-    };
+
+    }
 
     return (
-        <RoundButton clicked={isClicked} onClick={SetPosition}>
+        <RoundButton clicked={isClicked} onClick={setPosition}>
             <CrossHairImage src={CrossHairLogo}/>
         </RoundButton>
     )
@@ -41,14 +55,15 @@ const RoundButton = styled.button<{clicked: boolean}>`
     height: 4em;
     border-radius: 4em;
     box-shadow: 1px 1px 3px rgb(0, 0, 0, 0.15);
-    background-color: ${(props) => props.clicked? "green":"white"};
+    background-color: ${(props) => props.clicked? "#87DEBE":"white"};
     position: absolute;
-    z-index: 10;
+    z-index: 5;
     bottom: 7em;
     right: 2em;
     transition: background-color 0.5s ease-in-out;
 
 `;
+
 
 
 export default PanButton;
